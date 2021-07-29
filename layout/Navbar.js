ba40043ba58cfaaca85f46d58e70b4c80ddb9a39
Martin Ellis <martinryanellis@gmail.com>
2021-07-29 19:02:06 +0100

    Tailwind evaluation bug - fixed

--- layout/Navbar.js
+++ layout/Navbar.js
@@ -15,19 +15,15 @@ const Navbar = () => {
 
     return (
         <div className="fixed w-full flex justify-between items-center px-5 md:px-10 xl:px-40 py-3 bg-none z-10">
-            <div>
-                <Link href="/">
-                    <a>
-                        <BackIcon className={isHome ? "invisible" : "visible"}/>
-                    </a>
-                </Link>
-            </div>
+            <Link href="/">
+                <a>
+                    <BackIcon className={isHome ? "invisible" : "visible"}/>
+                </a>
+            </Link>
             <div className="w-20 lg:w-24 flex items-center">
                 <img src="/fanta-logo.png" className="max-w-full"/>
             </div>
-            <div>
-                <DarkModeIcon handleClick={() => setTheme(colorTheme)}/>
-            </div>
+            <DarkModeIcon handleClick={() => setTheme(colorTheme)}/>
         </div>
     );
 }
